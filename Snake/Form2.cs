02d5f7a33ed82d8fd0014f56c959a2b20eab2318
Stefan677890 <szymonmisztal2626@gmail.com>
2026-05-22 17:43:01 +0200

    Version 0.7
    Fixed "try again" button, added spawning apples and their reaction when a snake touches them and also added speed increase by 0.005% when snake eats an apple (it stacks overtime)

--- Snake/Form2.cs
+++ Snake/Form2.cs
@@ -12,86 +12,96 @@ namespace Snake
 
         int PlayerX = 377;
         int PlayerY = 304;
-        int PlayerSpeed = 0;
+
+        
+        double PlayerSpeed = 0;
 
         int AppleX;
         int AppleY;
+
+        Random rand = new Random();
+
         string direction = "right";
 
         public Form2()
         {
             InitializeComponent();
 
+            this.KeyPreview = true;
+
             if (difficulty == 0)
             {
-                PlayerSpeed = 4;
+                PlayerSpeed = 4.0;
             }
             else if (difficulty == 1)
             {
-                PlayerSpeed = 5;
+                PlayerSpeed = 5.0;
             }
             else if (difficulty == 2)
             {
-                PlayerSpeed = 6;
+                PlayerSpeed = 6.0;
             }
             else if (difficulty == 3)
             {
-                PlayerSpeed = 7;
+                PlayerSpeed = 7.0;
             }
-
 
             this.DoubleBuffered = true;
         }
 
         private void Tick(object sender, EventArgs e)
         {
+ 
             if (direction == "up")
             {
-                PlayerY -= PlayerSpeed;
+                PlayerY -= (int)PlayerSpeed;
             }
             else if (direction == "down")
             {
-                PlayerY += PlayerSpeed;
+                PlayerY += (int)PlayerSpeed;
             }
             else if (direction == "left")
             {
-                PlayerX -= PlayerSpeed;
+                PlayerX -= (int)PlayerSpeed;
             }
             else if (direction == "right")
             {
-                PlayerX += PlayerSpeed;
+                PlayerX += (int)PlayerSpeed;
             }
 
             this.Invalidate();
 
+            Rectangle snakeRect = new Rectangle(PlayerX, PlayerY, 20, 20);
+            Rectangle appleRect = new Rectangle(AppleX, AppleY, 20, 20);
+
+            if (snakeRect.IntersectsWith(appleRect))
+            {
+                PlayerScore += 1;
+                PlayerSpeed *= 1.005;      
+                SpawnApple();
+            }
+
+
             if (PlayerX > this.ClientSize.Width - 140)
             {
-                PlayerX = 377;
-                PlayerY = 304;
                 Timer.Stop();
                 try_again.Show();
                 Main_menu.Show();
             }
             if (PlayerX < 117)
             {
-                PlayerX = 377;
-                PlayerY = 304;
                 Timer.Stop();
                 try_again.Show();
                 Main_menu.Show();
             }
             if (PlayerY > this.ClientSize.Height - 50)
             {
-                PlayerX = 377;
-                PlayerY = 304;
                 Timer.Stop();
                 try_again.Show();
                 Main_menu.Show();
             }
             if (PlayerY < 108)
             {
-                PlayerX = 377;
-                PlayerY = 304;
                 Timer.Stop();
                 try_again.Show();
                 Main_menu.Show();
@@ -101,6 +111,7 @@ namespace Snake
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillRectangle(Brushes.Green, new Rectangle(PlayerX, PlayerY, 20, 20));
+            e.Graphics.FillRectangle(Brushes.Red, new Rectangle(AppleX, AppleY, 20, 20));
         }
 
         private void Form2_KeyDown(object sender, KeyEventArgs e)
@@ -119,18 +130,39 @@ namespace Snake
             Timer.Start();
             try_again.Hide();
             Main_menu.Hide();
+            SpawnApple();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            Board menu = new Board();
+            menu.Show();
+            this.Close();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Timer.Start();
+            PlayerX = 377;
+            PlayerY = 304;
+            direction = "right";
+
             try_again.Hide();
             Main_menu.Hide();
+            Timer.Start();
+
+            this.Focus();
+        }
+
+        private void SpawnApple()
+        {
+            int minX = 117;
+            int maxX = this.ClientSize.Width - 160;
+
+            int minY = 108;
+            int maxY = this.ClientSize.Height - 70;
+
+            AppleX = rand.Next(minX, maxX);
+            AppleY = rand.Next(minY, maxY);
         }
     }
 }
